f403b259ad81946d3be7450bd7a8421f0fe71ce8
Sebastian Andres <sebastianandres@MacBook-Air-de-Sebastian.local>
2021-05-01 03:45:56 -0400

    validation_pdf and download_pdf_viewGame

--- app/models/rule.rb
+++ app/models/rule.rb
@@ -1,5 +1,15 @@
 class Rule < ApplicationRecord
     belongs_to :game
     has_one_attached :document_pdf
-    # validates_attachment :document_pdf, attached: true, content_type: { in: 'application/pdf', message: 'This is not a PDF file' }
+    
+    # Se crea una validacion customizada
+    validate :correct_document_mime_type
+
+    private
+
+  def correct_document_mime_type
+    if document_pdf.attached? && !document_pdf.content_type.in?(%w(application/msword application/pdf))
+      errors.add(:document_pdf, 'Must be a PDF or a DOC file')
+    end
+  end
 end
